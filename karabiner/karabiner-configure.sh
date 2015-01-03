11495be00151c43e28d9b76ee58a2eec1f02529b
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2015-01-03 13:36:38 -0600

    add tmux bindings to firefox

--- karabiner/karabiner-configure.sh
+++ karabiner/karabiner-configure.sh
@@ -12,7 +12,13 @@ $cli set parameter.mousekey_repeat_wait_of_scroll 5
 /bin/echo -n .
 $cli set parameter.maximum_speed_of_scroll 5
 /bin/echo -n .
+$cli set parameter.simultaneouskeypresses_delay 100
+/bin/echo -n .
 $cli set parameter.mousekey_high_speed_of_scroll 10
+/bin/echo -n .
+$cli set parameter.mousekey_initial_wait_of_scroll 5
+/bin/echo -n .
+$cli set remap.app_firefox_tmux 1
 /bin/echo -n .
 $cli set parameter.mousekey_repeat_wait_of_pointer 5
 /bin/echo -n .
@@ -24,8 +30,8 @@ $cli set repeat.consumer_wait 10
 /bin/echo -n .
 $cli set parameter.blockuntilkeyup_timeout 0
 /bin/echo -n .
-$cli set parameter.mousekey_initial_wait_of_scroll 5
-/bin/echo -n .
 $cli set remap.mouse_navigation 1
+/bin/echo -n .
+$cli set parameter.wait_before_and_after_a_modifier_key_event 30
 /bin/echo -n .
 /bin/echo
