92001df3c4773a132186412cc5b2869a98d3809e
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2016-11-16 11:18:21 -0600

    update ruby versions

--- rvm/base.sh
+++ rvm/base.sh
@@ -1,7 +1,5 @@
 source "$HOME/.rvm/scripts/rvm"
-rvm install ruby-1.9
-rvm install ruby-2.0
-rvm install ruby-2.1
-rvm install ruby-2.2
-rvm --default use ruby-2.2
+rvm install ruby-2.3
+rvm install ruby-2.4
+rvm --default use ruby-2.3
 gem install bundler rake rspec rails --no-rdoc --no-ri
